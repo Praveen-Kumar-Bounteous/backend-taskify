61eb86666d7e91eb30d01b8970112d954e594435
Praveen Kumar V <praveen.kumar2@bounteous.com>
2026-03-01 14:09:21 +0530

    Handled endpoint (/)

--- src/server.ts
+++ src/server.ts
@@ -17,3 +17,16 @@ app.listen(PORT, () => {
   console.log("/api/tasks - PUT(/:id) - To update task by ID");
   console.log("/api/tasks - DELETE(/:id) - To delete/remove task by ID");
 });
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1> Taskify Backend API </h1>
+    <ul>
+      <li><b>GET</b> /api/tasks</li>
+      <li><b>POST</b> /api/tasks</li>
+      <li><b>GET</b> /api/tasks/:id</li>
+      <li><b>PUT</b> /api/tasks/:id</li>
+      <li><b>DELETE</b> /api/tasks/:id</li>
+    </ul>
+  `);
+});
