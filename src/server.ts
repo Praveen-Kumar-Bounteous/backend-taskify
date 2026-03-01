31964306f8b780e66a822ca9fccfb435a18a5b97
Praveen Kumar V <praveen.kumar2@bounteous.com>
2026-03-01 14:02:27 +0530

    Added Endpoints in console for readability

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,9 @@ connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("/api/tasks - GET(/) - To list all tasks");
+  console.log("/api/tasks - POST(/) - To add tasks");
+  console.log("/api/tasks - GET(/:id) - To fetch task by ID");
+  console.log("/api/tasks - PUT(/:id) - To update task by ID");
+  console.log("/api/tasks - DELETE(/:id) - To delete/remove task by ID");
 });
